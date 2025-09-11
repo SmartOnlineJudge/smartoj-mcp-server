66c4ffe850dfc6c3cc3385c8ca3bc86dba533ea3
Rainbow <1834763300@qq.com>
2025-09-11 10:37:49 +0800

    feat: 查询一个题目的相关信息
    1. 指定题目ID的判题模板
    2. 指定题目ID的测试用例
    3. 指定题目ID的解题框架
    4. 指定题目ID的内存时间限制

--- tools/question/connector.py
+++ tools/question/connector.py
@@ -7,3 +7,14 @@ class QuestionBackendConnector(BaseBackendConnector):
 
     async def query_all_tags(self):
         return await self.client.get("/question/tags")
+
+    async def query_question_related_data(self, question_id: int, related_type: str):
+        """
+        查询一个题目的所有关联数据。
+
+        Args:
+            question_id (int): 题目的id
+            related_type (str): 关联数据的类型（judge-templates | memory-time-limits | memory-time-limits | tests | sovling-frameworks）
+        """
+        params = {"question_id": question_id}
+        return await self.client.get(f"/question/{related_type}", params=params)
--- tools/question/tool.py
+++ tools/question/tool.py
@@ -58,3 +58,109 @@ async def query_all_tags():
         tag.pop("created_at")
         results.append(tag)
     return results
+
+
+@question_mcp.tool
+async def query_judge_templates_of_question(question_id: int):
+    """
+    查询一个题目的所有判题模板信息。
+
+    在调用这个工具时你需要输入一个参数：
+    - question_id: 题目的id
+
+    你可以通过这个工具查询到数据库中指定题目的所有评测模板的详细信息。
+
+    一个判题模板通常包括以下字段：
+    - id: 该评测模板在数据库中的唯一标识
+    - code: 判题模板的代码
+    - language: 评测模板对应的编程语言（对象类型）
+      - id: 该编程语言在数据库中的唯一标识
+      - name: 编程语言的名称
+    """
+    response = await question_connector.query_question_related_data(question_id, "judge-templates")
+    try:
+        data = response["data"]
+    except KeyError:
+        return "获取判题模板失败"
+    if not data:
+        return "该题目暂时没有判题模板"
+    return data
+
+
+@question_mcp.tool
+async def query_memory_time_limits_of_question(question_id: int):
+    """
+    查询一个题目的内存时间限制。
+
+    在调用这个工具时你需要输入一个参数：
+    - question_id: 题目的id
+
+    你可以通过这个工具查询到数据库中指定题目的内存时间限制。
+
+    一个内存时间限制通常包括以下字段：
+    - id: 该内存时间限制在数据库中的唯一标识
+    - memory_limit: 题目的内存限制(单位 MB)
+    - time_limit: 题目的时间限制(单位 ms)
+    - language: 评测模板对应的编程语言（对象类型）
+      - id: 该编程语言在数据库中的唯一标识
+      - name: 编程语言的名称
+    """
+    response = await question_connector.query_question_related_data(question_id, "memory-time-limits")
+    try:
+        data = response["data"]
+    except KeyError:
+        return "获取内存时间限制失败"
+    if not data:
+        return "该题目暂时没有内存时间限制"
+    return data
+
+
+@question_mcp.tool
+async def query_solving_frameworks_of_question(question_id: int):
+    """
+    查询一个题目的解题框架信息。
+
+    在调用这个工具时你需要输入一个参数：
+    - question_id: 题目的id
+
+    你可以通过这个工具查询到数据库中指定题目的所有解题框架的详细信息。
+
+    一个解题框架通常包括以下字段：
+    - id: 该解题框架在数据库中的唯一标识
+    - code_framework: 解题框架的代码
+    - language: 评测模板对应的编程语言（对象类型）
+      - id: 该编程语言在数据库中的唯一标识
+      - name: 编程语言的名称
+    """
+    response = await question_connector.query_question_related_data(question_id, "solving-frameworks")
+    try:
+        data = response["data"]
+    except KeyError:
+        return "获取解题框架失败"
+    if not data:
+        return "该题目暂时没有解题框架"
+    return data
+
+
+@question_mcp.tool
+async def query_tests_of_question(question_id: int):
+    """
+    查询一个题目的所有测试用例信息。
+
+    在调用这个工具时你需要输入一个参数：
+    - question_id: 题目的id
+
+    你可以通过这个工具查询到数据库中指定题目的所有测试用例的详细信息。
+
+    一个测试用例通常包括以下字段：
+    - id: 该测试用例在数据库中的唯一标识
+    - input_output: 测试用例的原始输入输出信息
+    """
+    response = await question_connector.query_question_related_data(question_id, "tests")
+    try:
+        data = response["data"]
+    except KeyError:
+        return "获取测试用例失败"
+    if not data:
+        return "该题目暂时没有测试用例"
+    return data
